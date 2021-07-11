571d60d719ab028d2573f9376a16b84d7d406979
Takashi.Horiuchi <takashi_r_0902@yahoo.co.jp>
2021-07-11 15:06:54 +0900

    429 fix

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from pandas as pd
 from menta_scraping import MentaScraping
 from spread_sheet_api import SpreadSheetAPI
 from kick_parts import Parts
@@ -8,25 +9,30 @@ from kick_parts import Parts
 menta_html_client = MentaScraping()
 def_parts = Parts()
 
+# menta情報を格納するDataFrame
+menta_df = pd.DataFrame()
+
 page = 1
 while page <= 100:
     # MENTA MSG HTML情報を取得  TODO:本当はマルチスレッドを起動して複数ページに同時アクセスするようにしたい
     msg_html_info = menta_html_client.get_msg_html_info(page)
 
     # メッセージ情報を取得
-    menta_df = menta_html_client.get_msg_data_info(msg_html_info)
+    menta_df_tmp = menta_html_client.get_msg_data_info(msg_html_info)
 
     # 情報取得できなかった時点で終了
-    if len(menta_df) > 0:
+    if len(menta_df_tmp) > 0:
+        # 取得ページ毎に情報を積上
+        menta_df = pd.concat([menta_df, menta_df_tmp])
         page += 1
     else:
         break
 
-    # 契約中だけれど、SSに名前の無い方を追加
-    def_parts.insert_name(menta_df)
+# 契約中だけれど、SSに名前の無い方を追加
+def_parts.insert_name(menta_df)
 
-    # SSに最終連絡日を更新
-    def_parts.update_date(menta_df)
+# SSに最終連絡日を更新
+def_parts.update_date(menta_df)
 
 # MENTA CONTRACT HTMLを取得
 cont_html_info = menta_html_client.get_cont_html_info()
--- test_main.py
+++ test_main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import pandas as pd
 from menta_scraping import MentaScraping
 from spread_sheet_api import SpreadSheetAPI
 from kick_parts import Parts
@@ -8,30 +8,35 @@ from kick_parts import Parts
 menta_html_client = MentaScraping()
 def_parts = Parts()
 
+# menta情報を格納するDataFrame
+menta_df = pd.DataFrame()
+
+# Scripingするページ数
 page = 1
 while page <= 1:
     # html情報を取得
     from bs4 import BeautifulSoup
-    msg_html_info = BeautifulSoup(open('MENTA_20200915.html'), 'html.parser')
+    msg_html_info = BeautifulSoup(open('MENTA_msg.html'), 'html.parser')
 
     # menta情報を取得
-    menta_df = menta_html_client.get_msg_data_info(msg_html_info)
+    menta_df_tmp = menta_html_client.get_msg_data_info(msg_html_info)
 
     # 情報取得できなかった時点で終了
     if page == 1:
+        menta_df = pd.concat([menta_df, menta_df_tmp])
         page += 1
     else:
         break
 
-    # 契約中だけれど、SSに名前の無い方を追加
-    def_parts.insert_name(menta_df)
+# 契約中だけれど、SSに名前の無い方を追加
+def_parts.insert_name(menta_df)
 
-    # SSに最終連絡日を更新
-    def_parts.update_date(menta_df)
+# SSに最終連絡日を更新
+def_parts.update_date(menta_df)
 
 # MENTA CONTRACT HTMLを取得
 from bs4 import BeautifulSoup
-cont_html_info = BeautifulSoup(open('menta_cont_list.html'), 'html.parser')
+cont_html_info = BeautifulSoup(open('MENTA_cont.html'), 'html.parser')
 # 契約者情報を取得
 cont_df = menta_html_client.get_cont_data_info(cont_html_info)
 
